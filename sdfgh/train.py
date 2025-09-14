8318d2c23cf1b5836fc1ec2c724192f682b53fe1
Jeevan R P <jeevanrp1406@gmail.com>
2025-09-14 21:53:34 +0530

    Delete sdfgh directory

--- sdfgh/train.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-def train_model(model, datatrain, datavalid, epochs=15):
-    early = tf.keras.callbacks.EarlyStopping(
-        monitor='val_loss', patience=3, restore_best_weights=True
-    )
-    history = model.fit(datatrain, validation_data=datavalid, epochs=epochs, callbacks=[early])
-    return history
